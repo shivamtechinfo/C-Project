174c49a63d3a83d4ccd26629386aeaa0bd1e3b6d
shivamtechinfo <168769050+shivamtechinfo@users.noreply.github.com>
2024-06-09 23:56:06 +0530

    Add files via upload

---
+++ cp29.c
@@ -0,0 +1,16 @@
+//print alphabet from a to z using while loop
+#include<stdio.h>
+int main() {
+    
+    char ch = 'a';
+
+    while (ch  <= 'z')
+    {
+        printf("%c ", ch);
+        ch ++;
+       
+    }
+    
+
+    return 0;
+}
