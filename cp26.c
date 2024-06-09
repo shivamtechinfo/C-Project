671d272107f9e5d56167deffe842b1ee722d0318
shivamtechinfo <168769050+shivamtechinfo@users.noreply.github.com>
2024-06-09 23:37:43 +0530

    Add files via upload

---
+++ cp26.c
@@ -0,0 +1,16 @@
+//print n natural number
+#include<stdio.h>
+int main() {
+    int num;
+
+    printf("Enter a number:");
+    scanf("%d", &num);
+
+    for(int i = 1; i <= num; i++){
+
+        printf("%d\n", i);
+
+    }
+
+    return 0;
+}
